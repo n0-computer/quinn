9c490aa755d99aaacde858238a6b91c2d571874a
Diva M <divma@protonmail.com>
2024-08-01 16:48:56 -0500

    tweak example to start testing

--- quinn/examples/client.rs
+++ quinn/examples/client.rs
@@ -13,7 +13,7 @@ use std::{
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
-use proto::crypto::rustls::QuicClientConfig;
+use proto::{crypto::rustls::QuicClientConfig, TransportConfig};
 use rustls::pki_types::CertificateDer;
 use tracing::{error, info};
 use url::Url;
@@ -101,8 +101,13 @@ async fn run(options: Opt) -> Result<()> {
         client_crypto.key_log = Arc::new(rustls::KeyLogFile::new());
     }
 
-    let client_config =
+    let mut transport = TransportConfig::default();
+    transport
+        .report_observed_addresses_to_peers(true)
+        .accept_observed_address_reports(true);
+    let mut client_config =
         quinn::ClientConfig::new(Arc::new(QuicClientConfig::try_from(client_crypto)?));
+    client_config.transport_config(Arc::new(transport));
     let mut endpoint = quinn::Endpoint::client(options.bind)?;
     endpoint.set_default_client_config(client_config);
 
