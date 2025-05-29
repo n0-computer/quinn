964d145af2da30139269cb975a1a845053c892ae
Philipp Krüger <philipp.krueger1@gmail.com>
2025-05-29 15:02:47 +0200

    Test new virtualnet stuff

--- quinn/tests/web_test.rs
+++ quinn/tests/web_test.rs
@@ -1,17 +1,10 @@
-use std::{
-    collections::{BTreeMap, VecDeque},
-    net::SocketAddr,
-    pin::Pin,
-    sync::{Arc, Mutex},
-    task::{ready, Context, Poll, Waker},
-};
+use std::sync::Arc;
 
-use bytes::Bytes;
-use iroh_quinn::{AsyncUdpSocket, Endpoint, Runtime as _};
+use iroh_quinn::{
+    virtualnet::{switch::Switch, TestAddr},
+    Endpoint, Runtime as _,
+};
 use proto::{ClientConfig, EndpointConfig, ServerConfig};
-use tokio::sync::mpsc;
-use tokio_util::{sync::PollSender, task::AbortOnDropHandle};
-use udp::{EcnCodepoint, Transmit};
 
 #[tokio::test]
 async fn test_connect_with_virtual_socket() {
@@ -19,12 +12,10 @@ async fn test_connect_with_virtual_socket() {
     let runtime = Arc::new(iroh_quinn::TokioRuntime);
 
     // Virtual sockets setup
-    let mut net = VirtualNet::default();
-    let sock_a = net.add_socket(42);
-    let sock_b = net.add_socket(111);
-    let net = Arc::new(net);
-    let server_socket = VirtualSocket::new(&net, sock_a);
-    let client_socket = VirtualSocket::new(&net, sock_b);
+    let switch = Switch::new();
+    let server_socket = switch.connect_socket(TestAddr(42)).await;
+    let client_socket = switch.connect_socket(TestAddr(111)).await;
+    let server_addr = server_socket.addr;
 
     // Cert setup
     let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
@@ -38,14 +29,14 @@ async fn test_connect_with_virtual_socket() {
     let server_ep = Endpoint::new_with_abstract_socket(
         EndpointConfig::default(),
         Some(server_config),
-        server_socket.clone(),
+        Box::new(server_socket),
         runtime.clone(),
     )
     .unwrap();
     let mut client_ep = Endpoint::new_with_abstract_socket(
         EndpointConfig::default(),
         None,
-        client_socket.clone(),
+        Box::new(client_socket),
         runtime.clone(),
     )
     .unwrap();
@@ -63,39 +54,39 @@ async fn test_connect_with_virtual_socket() {
         }
     }));
 
-    server_socket.set_paused(true);
-    let mut wiretap = server_socket.wiretap();
+    // server_socket.set_paused(true);
+    // let mut wiretap = server_socket.wiretap();
 
-    let (conn, to_resend) = tokio::join!(
-        async {
-            // Connect from the client
-            let conn = client_ep
-                .connect(server_socket.addr, "localhost")
-                .unwrap()
-                .await
-                .unwrap();
-            conn
-        },
-        async {
-            // Grab the last
-            let mut to_resend = wiretap.recv().await.unwrap();
-            to_resend.src_ip = SocketAddr::new([192, 168, 0, 133].into(), 1234);
-            server_socket
-                .try_send(&to_resend.as_quinn_transmit())
-                .expect("couldn't resend packet");
-            server_socket.set_paused(false);
-            to_resend
-        }
-    );
+    // let (conn, to_resend) = tokio::join!(
+    //     async {
+    // Connect from the client
+    let conn = client_ep
+        .connect(server_addr, "localhost")
+        .unwrap()
+        .await
+        .unwrap();
+    //         conn
+    //     },
+    //     async {
+    //         // Grab the last
+    //         let mut to_resend = wiretap.recv().await.unwrap();
+    //         to_resend.src_ip = SocketAddr::new([192, 168, 0, 133].into(), 1234);
+    //         server_socket
+    //             .try_send(&to_resend.as_quinn_transmit())
+    //             .expect("couldn't resend packet");
+    //         server_socket.set_paused(false);
+    //         to_resend
+    //     }
+    // );
 
     conn.closed().await;
 
     client_ep.close(1u32.into(), b"endpoint closed");
     server_ep.close(1u32.into(), b"endpoint closed");
 
-    client_socket
-        .try_send(&to_resend.as_quinn_transmit())
-        .expect("couldn't resend packet");
+    // client_socket
+    //     .try_send(&to_resend.as_quinn_transmit())
+    //     .expect("couldn't resend packet");
 
     client_ep.wait_idle().await;
     server_ep.wait_idle().await;
