5b2b17c9c68de5006736ef45cb2fb0804a3ac2ec
Diva Martínez <divma@protonmail.com>
2025-10-24 15:55:37 -0500

    add RemoveAddress frame

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -4362,8 +4362,10 @@ impl Connection {
                 Frame::AddAddress(_addr) => {
                     // TODO(@divma): handle
                 }
-
                 Frame::PunchMeNow(_frame) => {
+                    // TODO(@divma): handle
+                }
+                Frame::RemoveAddress(_frame) => {
                     // TODO(@divma): handle
                 }
             }
