46a695d3b4199492346bda07746513847a64592c
divagant-martian <divagant-martian@users.noreply.github.com>
2025-12-16 21:08:46 +0000

    deploy: 0f6b997e07f949c501cbab2357b1ce14161c2ff4

---
+++ pr/271/docs/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn_proto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"iroh_quinn_proto/struct.ConnectionId.html\" title=\"struct iroh_quinn_proto::ConnectionId\">ConnectionId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[324]}
