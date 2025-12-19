fdcf56f13c736a97262ca5b1cc5918f3d426a6ab
divagant-martian <divagant-martian@users.noreply.github.com>
2025-12-19 20:35:14 +0000

    deploy: c430aa4a67a63bca09aac50e8f34782c1e970c2c

---
+++ pr/292/docs/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl Args for <a class=\"struct\" href=\"bench/struct.Opt.html\" title=\"struct bench::Opt\">Opt</a>"]]],["perf",[["impl Args for <a class=\"struct\" href=\"perf/client/struct.Opt.html\" title=\"struct perf::client::Opt\">Opt</a>"],["impl Args for <a class=\"struct\" href=\"perf/server/struct.Opt.html\" title=\"struct perf::server::Opt\">Opt</a>"],["impl Args for <a class=\"struct\" href=\"perf/struct.CommonOpt.html\" title=\"struct perf::CommonOpt\">CommonOpt</a>"]]],["quinn_perf",[["impl Args for <a class=\"struct\" href=\"quinn_perf/struct.Cli.html\" title=\"struct quinn_perf::Cli\">Cli</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[116,368,132]}
