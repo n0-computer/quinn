94031d5354caa6dddc3e2c8db7fec07e55e5db48
Philipp Krüger <philipp.krueger1@gmail.com>
2026-01-13 20:23:46 +0100

    fix(proto): Check `abandoned_paths` in `open_path_ensure`.

--- quinn-proto/src/connection/mod.rs
+++ quinn-proto/src/connection/mod.rs
@@ -569,16 +569,13 @@ impl Connection {
         initial_status: PathStatus,
         now: Instant,
     ) -> Result<(PathId, bool), PathError> {
-        Ok(
-            match self
-                .paths
-                .iter()
-                .find(|(_id, path)| network_path.is_probably_same_path(&path.data.network_path))
-            {
-                Some((path_id, _state)) => (*path_id, true),
-                None => (self.open_path(network_path, initial_status, now)?, false),
-            },
-        )
+        match self.paths.iter().find(|(id, path)| {
+            network_path.is_probably_same_path(&path.data.network_path)
+                && !self.abandoned_paths.contains(*id)
+        }) {
+            Some((path_id, _state)) => Ok((*path_id, true)),
+            None => Ok((self.open_path(network_path, initial_status, now)?, false)),
+        }
     }
 
     /// Opens a new path
