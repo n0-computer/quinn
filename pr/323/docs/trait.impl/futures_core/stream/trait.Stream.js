6f2d3c7efd4b12c96d86c6bf1008d721e1f7eb1c
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-01-14 12:38:52 +0000

    deploy: e217718d375585e0ec235f8b21bd30c4d6d4cecd

---
+++ pr/323/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn",[["impl Stream for <a class=\"struct\" href=\"iroh_quinn/struct.AddressDiscovery.html\" title=\"struct iroh_quinn::AddressDiscovery\">AddressDiscovery</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[172]}
