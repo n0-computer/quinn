2c4be7562e9494481c66cef2e7c6dc792320588f
rklaehn <rklaehn@users.noreply.github.com>
2026-01-14 14:23:52 +0000

    deploy: b3d3b6088da81af3697970d155edc897eaf80d1c

---
+++ pr/325/docs/trait.impl/arbitrary/trait.Arbitrary.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn_proto",[["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"enum\" href=\"iroh_quinn_proto/enum.Dir.html\" title=\"enum iroh_quinn_proto::Dir\">Dir</a>"],["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"enum\" href=\"iroh_quinn_proto/enum.PathStatus.html\" title=\"enum iroh_quinn_proto::PathStatus\">PathStatus</a>"],["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"enum\" href=\"iroh_quinn_proto/enum.Side.html\" title=\"enum iroh_quinn_proto::Side\">Side</a>"],["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"struct\" href=\"iroh_quinn_proto/struct.PathId.html\" title=\"struct iroh_quinn_proto::PathId\">PathId</a>"],["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"struct\" href=\"iroh_quinn_proto/struct.StreamId.html\" title=\"struct iroh_quinn_proto::StreamId\">StreamId</a>"],["impl&lt;'arbitrary&gt; Arbitrary&lt;'arbitrary&gt; for <a class=\"struct\" href=\"iroh_quinn_proto/struct.VarInt.html\" title=\"struct iroh_quinn_proto::VarInt\">VarInt</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1069]}
