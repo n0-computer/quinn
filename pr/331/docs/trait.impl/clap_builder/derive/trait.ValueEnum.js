ce70605d25a2dcde4a744808a1519cff916bb602
flub <flub@users.noreply.github.com>
2026-01-15 12:33:41 +0000

    deploy: 69cb121d81da0a5cf4cda75559f9f6e911cddcb7

---
+++ pr/331/docs/trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["perf",[["impl ValueEnum for <a class=\"enum\" href=\"perf/enum.CongestionAlgorithm.html\" title=\"enum perf::CongestionAlgorithm\">CongestionAlgorithm</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[160]}
