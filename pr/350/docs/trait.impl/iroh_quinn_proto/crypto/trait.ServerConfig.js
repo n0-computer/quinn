32fcb5ea23fccb170de13e55e2c10a656918d0b2
matheus23 <matheus23@users.noreply.github.com>
2026-01-20 15:51:40 +0000

    deploy: a46f8f66e079cf1480967a9e62d2554c039a6055

---
+++ pr/350/docs/trait.impl/iroh_quinn_proto/crypto/trait.ServerConfig.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn",[]],["iroh_quinn_proto",[]],["perf",[["impl <a class=\"trait\" href=\"iroh_quinn_proto/crypto/trait.ServerConfig.html\" title=\"trait iroh_quinn_proto::crypto::ServerConfig\">ServerConfig</a> for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionServerConfig.html\" title=\"struct perf::noprotection::NoProtectionServerConfig\">NoProtectionServerConfig</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[17,24,347]}
