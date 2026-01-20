e0156bb532ec383b4a38b71c39dcd6229dc6751f
matheus23 <matheus23@users.noreply.github.com>
2026-01-20 15:56:43 +0000

    deploy: 6c3e67696b68347b093dd6155700f52672e40296

---
+++ pr/351/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["quinn_perf",[["impl Subcommand for <a class=\"enum\" href=\"quinn_perf/enum.Commands.html\" title=\"enum quinn_perf::Commands\">Commands</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[146]}
