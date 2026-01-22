0752c0a37c683d16b8bab96bcb1848bf7bb0388c
flub <flub@users.noreply.github.com>
2026-01-22 13:05:58 +0000

    deploy: 82ec87f5388e00027dc12c85c4f3a9fb75c4010f

---
+++ pr/358/docs/trait.impl/identity_hash/trait.IdentityHashable.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn_proto",[["impl IdentityHashable for <a class=\"struct\" href=\"iroh_quinn_proto/struct.PathId.html\" title=\"struct iroh_quinn_proto::PathId\">PathId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[170]}
