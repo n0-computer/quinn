9e57f6cbaa1597cb3952f8f199989387098598cd
matheus23 <matheus23@users.noreply.github.com>
2026-01-26 11:33:21 +0000

    deploy: dd6a25e6a928b5af3c1788e22de29440c2a4ce45

---
+++ pr/366/docs/trait.impl/clap_builder/derive/trait.Parser.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl Parser for <a class=\"struct\" href=\"bench/struct.Opt.html\" title=\"struct bench::Opt\">Opt</a>"]]],["perf",[["impl Parser for <a class=\"struct\" href=\"perf/client/struct.Opt.html\" title=\"struct perf::client::Opt\">Opt</a>"],["impl Parser for <a class=\"struct\" href=\"perf/server/struct.Opt.html\" title=\"struct perf::server::Opt\">Opt</a>"],["impl Parser for <a class=\"struct\" href=\"perf/struct.CommonOpt.html\" title=\"struct perf::CommonOpt\">CommonOpt</a>"]]],["quinn_perf",[["impl Parser for <a class=\"struct\" href=\"quinn_perf/struct.Cli.html\" title=\"struct quinn_perf::Cli\">Cli</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[118,374,134]}
