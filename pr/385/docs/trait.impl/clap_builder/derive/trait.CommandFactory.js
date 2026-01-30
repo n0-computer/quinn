9e037bbab2c78525fa1496ff603f70e75891e7b9
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-01-30 13:01:38 +0000

    deploy: ca9029fda66dc3d9b0615727a3e63d971360c0dc

---
+++ pr/385/docs/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl CommandFactory for <a class=\"struct\" href=\"bench/struct.Opt.html\" title=\"struct bench::Opt\">Opt</a>"]]],["perf",[["impl CommandFactory for <a class=\"struct\" href=\"perf/client/struct.Opt.html\" title=\"struct perf::client::Opt\">Opt</a>"],["impl CommandFactory for <a class=\"struct\" href=\"perf/server/struct.Opt.html\" title=\"struct perf::server::Opt\">Opt</a>"],["impl CommandFactory for <a class=\"struct\" href=\"perf/struct.CommonOpt.html\" title=\"struct perf::CommonOpt\">CommonOpt</a>"]]],["quinn_perf",[["impl CommandFactory for <a class=\"struct\" href=\"quinn_perf/struct.Cli.html\" title=\"struct quinn_perf::Cli\">Cli</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[126,398,142]}
