b715883570615590a7bb19e7760b276474b723c9
Arqu <Arqu@users.noreply.github.com>
2026-02-01 07:29:58 +0000

    deploy: c4c3bd67725453a08bd0e992cc50168bf9123612

---
+++ pr/390/docs/trait.impl/futures_io/if_std/trait.AsyncWrite.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn",[["impl AsyncWrite for <a class=\"struct\" href=\"iroh_quinn/struct.SendStream.html\" title=\"struct iroh_quinn::SendStream\">SendStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[158]}
