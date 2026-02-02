a8ef1c7ede5935454373a39857af3f8b8ec789e7
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-02-02 10:00:56 +0000

    deploy: 065fa5fab5532cb831c6f36edd98f1fa3339a4e8

---
+++ pr/392/docs/trait.impl/core/ops/index/trait.Index.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn_proto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"struct\" href=\"iroh_quinn_proto/struct.ConnectionHandle.html\" title=\"struct iroh_quinn_proto::ConnectionHandle\">ConnectionHandle</a>&gt; for Slab&lt;ConnectionMeta&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[361]}
