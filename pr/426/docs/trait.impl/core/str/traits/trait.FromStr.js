faa2fae2309c21f138494bbf70d0e32c4ac5ef71
Frando <Frando@users.noreply.github.com>
2026-02-13 10:08:46 +0000

    deploy: 4be9dd763ff1282670f048b61c2b6c610f4e0c36

---
+++ pr/426/docs/trait.impl/core/str/traits/trait.FromStr.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"bench/enum.CipherSuite.html\" title=\"enum bench::CipherSuite\">CipherSuite</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[281]}
