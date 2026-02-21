f47506b570d3dcab6824b1540b6f39ad92d73821
divagant-martian <divagant-martian@users.noreply.github.com>
2026-02-21 00:29:23 +0000

    deploy: a3e8e9e4515d123f895ed15a30a1ba4378b62f7c

---
+++ pr/445/docs/trait.impl/clap_builder/derive/trait.FromArgMatches.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl FromArgMatches for <a class=\"struct\" href=\"bench/struct.Opt.html\" title=\"struct bench::Opt\">Opt</a>"]]],["perf",[["impl FromArgMatches for <a class=\"struct\" href=\"perf/client/struct.Opt.html\" title=\"struct perf::client::Opt\">Opt</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"perf/server/struct.Opt.html\" title=\"struct perf::server::Opt\">Opt</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"perf/struct.CommonOpt.html\" title=\"struct perf::CommonOpt\">CommonOpt</a>"]]],["quinn_perf",[["impl FromArgMatches for <a class=\"enum\" href=\"quinn_perf/enum.Commands.html\" title=\"enum quinn_perf::Commands\">Commands</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"quinn_perf/struct.Cli.html\" title=\"struct quinn_perf::Cli\">Cli</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[126,398,276]}
