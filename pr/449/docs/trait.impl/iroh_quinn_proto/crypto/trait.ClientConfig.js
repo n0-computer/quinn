be9c7dafa393155f8713d50f3ed3a2a9b4e1854f
matheus23 <matheus23@users.noreply.github.com>
2026-02-23 15:46:31 +0000

    deploy: 0c7696fc71f9c38b5f490529bd5aefd649aae79a

--- pr/449/docs/trait.impl/iroh_quinn_proto/crypto/trait.ClientConfig.js
+++ pr/449/docs/trait.impl/iroh_quinn_proto/crypto/trait.ClientConfig.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_quinn",[]],["iroh_quinn_proto",[]],["perf",[["impl <a class=\"trait\" href=\"iroh_quinn_proto/crypto/trait.ClientConfig.html\" title=\"trait iroh_quinn_proto::crypto::ClientConfig\">ClientConfig</a> for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionClientConfig.html\" title=\"struct perf::noprotection::NoProtectionClientConfig\">NoProtectionClientConfig</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh_quinn",[]],["iroh_quinn_proto",[]],["perf",[["impl ClientConfig for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionClientConfig.html\" title=\"struct perf::noprotection::NoProtectionClientConfig\">NoProtectionClientConfig</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[17,24,347]}
+//{"start":57,"fragment_lengths":[17,24,212]}
