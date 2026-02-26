2508742e0557f518704e15a2e1652edfd38ff000
rklaehn <rklaehn@users.noreply.github.com>
2026-02-26 10:11:31 +0000

    deploy: 48afec2528a963b8daebe09683175ad9db1b7713

---
+++ pr/454/docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_quinn_proto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"iroh_quinn_proto/struct.IncomingAlpns.html\" title=\"struct iroh_quinn_proto::IncomingAlpns\">IncomingAlpns</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[350]}
