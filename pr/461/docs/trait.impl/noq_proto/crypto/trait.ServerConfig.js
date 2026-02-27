b488503106c0338682bf8f94dc24fe06d8a22235
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-02-27 14:30:58 +0000

    deploy: 7f9bf04585597cc42ba1ee77fd11d50c32140029

--- pr/461/docs/trait.impl/noq_proto/crypto/trait.ServerConfig.js
+++ pr/461/docs/trait.impl/noq_proto/crypto/trait.ServerConfig.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["noq",[]],["noq_proto",[]],["perf",[["impl <a class=\"trait\" href=\"noq_proto/crypto/trait.ServerConfig.html\" title=\"trait noq_proto::crypto::ServerConfig\">ServerConfig</a> for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionServerConfig.html\" title=\"struct perf::noprotection::NoProtectionServerConfig\">NoProtectionServerConfig</a>"]]]]);
+    var implementors = Object.fromEntries([["noq",[]],["noq_proto",[]],["perf",[["impl ServerConfig for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionServerConfig.html\" title=\"struct perf::noprotection::NoProtectionServerConfig\">NoProtectionServerConfig</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[10,17,333]}
+//{"start":57,"fragment_lengths":[10,17,212]}
