3d9ff1baa535d0c2f0bee21f3f564e2fdf19e62f
matheus23 <matheus23@users.noreply.github.com>
2026-03-04 08:36:18 +0000

    deploy: 3826bc63a6f842d0b2e3fc92a4cd3dc2fd9a378d

--- pr/451/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
+++ pr/451/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["quinn_perf",[["impl Subcommand for <a class=\"enum\" href=\"quinn_perf/enum.Commands.html\" title=\"enum quinn_perf::Commands\">Commands</a>"]]]]);
+    var implementors = Object.fromEntries([["noq_perf",[["impl Subcommand for <a class=\"enum\" href=\"noq_perf/enum.Commands.html\" title=\"enum noq_perf::Commands\">Commands</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[146]}
+//{"start":57,"fragment_lengths":[140]}
--- pr/451/docs/trait.impl/tokio/io/async_write/trait.AsyncWrite.js
+++ pr/451/docs/trait.impl/tokio/io/async_write/trait.AsyncWrite.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_quinn",[["impl AsyncWrite for <a class=\"struct\" href=\"iroh_quinn/struct.SendStream.html\" title=\"struct iroh_quinn::SendStream\">SendStream</a>"]]]]);
+    var implementors = Object.fromEntries([["noq",[["impl AsyncWrite for <a class=\"struct\" href=\"noq/struct.SendStream.html\" title=\"struct noq::SendStream\">SendStream</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[158]}
+//{"start":57,"fragment_lengths":[137]}
