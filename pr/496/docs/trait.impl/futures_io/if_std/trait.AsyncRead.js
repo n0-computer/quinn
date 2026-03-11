4ddc831e26820e679aa24b56d242152eecb6bef6
matheus23 <matheus23@users.noreply.github.com>
2026-03-11 12:59:29 +0000

    deploy: fa8f7332aa71e2b905bd9814c5ba528283c05cab

---
+++ pr/496/docs/trait.impl/futures_io/if_std/trait.AsyncRead.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["noq",[["impl AsyncRead for <a class=\"struct\" href=\"noq/struct.RecvStream.html\" title=\"struct noq::RecvStream\">RecvStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[136]}
