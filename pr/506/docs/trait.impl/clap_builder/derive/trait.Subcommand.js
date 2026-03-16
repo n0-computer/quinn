5f0104f8c180eb7caa2b616476f1b9d796f2578d
flub <flub@users.noreply.github.com>
2026-03-16 09:10:27 +0000

    deploy: 87c1659bef645216356e6fd17ae66b5d70ed9e59

---
+++ pr/506/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["noq_perf",[["impl Subcommand for <a class=\"enum\" href=\"noq_perf/enum.Commands.html\" title=\"enum noq_perf::Commands\">Commands</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[140]}
