e79aac258c66cde94fba17fe6a37795baab265ab
matheus23 <matheus23@users.noreply.github.com>
2026-03-17 16:28:15 +0000

    deploy: 81949aa5669b4f634c4dfefed58dc04de37fb70e

---
+++ pr/511/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["noq",[["impl Stream for <a class=\"struct\" href=\"noq/struct.AddressDiscovery.html\" title=\"struct noq::AddressDiscovery\">AddressDiscovery</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[151]}
