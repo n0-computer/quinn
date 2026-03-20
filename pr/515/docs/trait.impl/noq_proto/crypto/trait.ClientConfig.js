753b5ca45272ba57df15c8da896d6d635b4a85ca
Frando <Frando@users.noreply.github.com>
2026-03-20 10:40:26 +0000

    deploy: c5947fb319764467d98b8f11a01c859b972d159c

--- pr/515/docs/trait.impl/noq_proto/crypto/trait.ClientConfig.js
+++ pr/515/docs/trait.impl/noq_proto/crypto/trait.ClientConfig.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["noq",[]],["noq_proto",[]],["perf",[["impl <a class=\"trait\" href=\"noq_proto/crypto/trait.ClientConfig.html\" title=\"trait noq_proto::crypto::ClientConfig\">ClientConfig</a> for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionClientConfig.html\" title=\"struct perf::noprotection::NoProtectionClientConfig\">NoProtectionClientConfig</a>"]]]]);
+    var implementors = Object.fromEntries([["noq",[]],["noq_proto",[]],["perf",[["impl ClientConfig for <a class=\"struct\" href=\"perf/noprotection/struct.NoProtectionClientConfig.html\" title=\"struct perf::noprotection::NoProtectionClientConfig\">NoProtectionClientConfig</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[10,17,333]}
+//{"start":57,"fragment_lengths":[10,17,212]}
