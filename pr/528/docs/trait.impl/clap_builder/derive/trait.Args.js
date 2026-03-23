e69bbf50f69ddde1a691462ad501bcf5b8188e67
Frando <Frando@users.noreply.github.com>
2026-03-23 08:40:49 +0000

    deploy: 2a1a18941c6cf2c160e55ed7bf3637655eacb26e

---
+++ pr/528/docs/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl Args for <a class=\"struct\" href=\"bench/struct.Opt.html\" title=\"struct bench::Opt\">Opt</a>"]]],["noq_perf",[["impl Args for <a class=\"struct\" href=\"noq_perf/struct.Cli.html\" title=\"struct noq_perf::Cli\">Cli</a>"]]],["perf",[["impl Args for <a class=\"struct\" href=\"perf/client/struct.Opt.html\" title=\"struct perf::client::Opt\">Opt</a>"],["impl Args for <a class=\"struct\" href=\"perf/server/struct.Opt.html\" title=\"struct perf::server::Opt\">Opt</a>"],["impl Args for <a class=\"struct\" href=\"perf/struct.CommonOpt.html\" title=\"struct perf::CommonOpt\">CommonOpt</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[116,126,368]}
