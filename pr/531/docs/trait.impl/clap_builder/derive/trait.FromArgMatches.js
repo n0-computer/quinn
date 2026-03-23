7bbe0ef17b479288b13d43048755f3b007a81962
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-03-23 12:01:14 +0000

    deploy: 84b0904e29a2e5d3aa2930d77e025b196a172f30

---
+++ pr/531/docs/trait.impl/clap_builder/derive/trait.FromArgMatches.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bench",[["impl FromArgMatches for <a class=\"struct\" href=\"bench/struct.Opt.html\" title=\"struct bench::Opt\">Opt</a>"]]],["noq_perf",[["impl FromArgMatches for <a class=\"enum\" href=\"noq_perf/enum.Commands.html\" title=\"enum noq_perf::Commands\">Commands</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"noq_perf/struct.Cli.html\" title=\"struct noq_perf::Cli\">Cli</a>"]]],["perf",[["impl FromArgMatches for <a class=\"struct\" href=\"perf/client/struct.Opt.html\" title=\"struct perf::client::Opt\">Opt</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"perf/server/struct.Opt.html\" title=\"struct perf::server::Opt\">Opt</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"perf/struct.CommonOpt.html\" title=\"struct perf::CommonOpt\">CommonOpt</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[126,266,398]}
