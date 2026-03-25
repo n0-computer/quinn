effa0a3b2a6a7dc2cb1f87c6805733163ef1c923
Diva Martínez <git@divma.net>
2026-03-25 12:11:20 -0500

    revert timer changes

--- noq-proto/src/connection/mod.rs
+++ noq-proto/src/connection/mod.rs
@@ -2398,21 +2398,11 @@ impl Connection {
                                 Timer::PerPath(path_id, PathTimer::PathChallengeLost),
                                 self.qlog.with_time(now),
                             );
+                            debug!("path migration validation failed");
                             if let Some((_, prev)) = path.prev.take() {
-                                debug!("path migration validation failed, reverting");
                                 path.data = prev;
-                                path.data.reset_on_path_challenges();
-                            } else {
-                                debug!("path revalidation failed, abandoning");
-                                path.data.reset_on_path_challenges();
-                                if let Err(err) = self.close_path_inner(
-                                    now,
-                                    path_id,
-                                    PathAbandonReason::ValidationFailed,
-                                ) {
-                                    warn!(?err, "failed closing path after revalidation failure");
-                                }
                             }
+                            path.data.reset_on_path_challenges();
                         }
                         PathTimer::PathChallengeLost => {
                             let Some(path) = self.paths.get_mut(&path_id) else {
@@ -6815,12 +6805,6 @@ impl Connection {
                     if let Some(path) = self.paths.get_mut(&path_id) {
                         path.data.pending_on_path_challenge = true;
                     }
-                    let pto = self.pto(SpaceKind::Data, path_id);
-                    self.timers.set(
-                        Timer::PerPath(path_id, PathTimer::PathValidationFailed),
-                        now + 3 * pto,
-                        self.qlog.with_time(now),
-                    );
                 }
                 Ok(Some((path_id, remote)))
             }
