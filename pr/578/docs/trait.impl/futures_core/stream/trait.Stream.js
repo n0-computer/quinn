d88fcad0f4bcd7b872bda7a730f6d7f9b652e8e6
matheus23 <matheus23@users.noreply.github.com>
2026-04-10 10:48:50 +0000

    deploy: aa2bd428f30e3c27a88d9eb928d64d0646b4c225

---
+++ pr/578/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["noq",[["impl Stream for <a class=\"struct\" href=\"noq/struct.AddressDiscovery.html\" title=\"struct noq::AddressDiscovery\">AddressDiscovery</a>"],["impl Stream for <a class=\"struct\" href=\"noq/struct.NatTraversalUpdates.html\" title=\"struct noq::NatTraversalUpdates\">NatTraversalUpdates</a>"],["impl Stream for <a class=\"struct\" href=\"noq/struct.ObservedExternalAddr.html\" title=\"struct noq::ObservedExternalAddr\">ObservedExternalAddr</a>"],["impl Stream for <a class=\"struct\" href=\"noq/struct.PathEvents.html\" title=\"struct noq::PathEvents\">PathEvents</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[580]}
