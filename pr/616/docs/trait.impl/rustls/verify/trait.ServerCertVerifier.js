b450c4b1bb3f8843a6e68bc31d0dd5bc21067033
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-04-27 10:38:57 +0000

    deploy: b114f871836756e58b99c128b4bfc0adde9ba2a8

---
+++ pr/616/docs/trait.impl/rustls/verify/trait.ServerCertVerifier.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["certificate",[["impl ServerCertVerifier for <a class=\"struct\" href=\"certificate/struct.SkipServerVerification.html\" title=\"struct certificate::SkipServerVerification\">SkipServerVerification</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[205]}
